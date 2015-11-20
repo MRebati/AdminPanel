52793a02f689968932320be3fc9373b56b6d590b
Mohammad Rebati <m.rebati@icloud.com>
2015-11-20 16:44:58 +0330

    responsiv

--- builds/Development/Scripts/script.js
+++ builds/Development/Scripts/script.js
@@ -44,11 +44,57 @@ return null!=s&&(a="number"==typeof s||"="!==s.charAt(1)?Number(s)-i:parseInt(s.
 $('#menu-icon').click(function(e){
   'use strict';
   e.preventDefault();
-  var container = $('container');
   $('aside').toggleClass('collapse',350);
-  $('aside > .details').fadeToggle();
-  $('.container').toggleClass('full-width',350);
 });
+
+$('#collapsed-icon').click(function(){
+  'use strict';
+  var width = $(window).width();
+  $('aside').toggleClass('showsidebar',350);
+  //TweenLite.to('aside', .7, { x: 500, ease: Power3.easeOut});
+  //$("aside").animate({width:'toggle'},350);
+});
+
+$(document).ready(function(){
+  'use strict';
+  var width = $(window).width();
+  if(width < 400){
+    $('aside').removeClass('collapse').hide();
+    $('.container').addClass('full');
+  }else{
+    $('aside').removeClass('showsidebar').show();
+    $('.container').removeClass('full');
+  }
+
+  if(width> 400 && width < 960){
+    'use strict';
+    $('aside').addClass('collapse');
+  }
+});
+
+$(window).resize(function(){
+  'use strict';
+  var width = $(window).width();
+  if(width < 400){
+    $('aside').removeClass('collapse').hide();
+  }else{
+    $('aside').removeClass('showsidebar').show();
+  }
+  if(width> 400 && width < 960){
+    'use strict';
+    $('aside').addClass('collapse');
+  }
+});
+
+/*function collabrate(){
+  'use strict';
+  var width = $(window).width();
+  var aside = $('aside');
+  var asidewidth = aside.width();
+  $('.container').width(width - asidewidth);
+  var containerwidth = $('.container').width();
+  //$('.container').css('width',containerwidth+17);
+}*/
 
 $(window).scroll(function () {
   'use strict';
